4fd11c1ba7c9e4fe4e25ef6df78963191fd6ba26
CheshiUwU <155390531+CheshiUwU@users.noreply.github.com>
2024-08-31 16:35:26 -0700

    passed most tests. need to make function return an empty string on input and error on negative input

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+let concatString = '';
+let result = "";
+const repeatString = function(string, num) {
+    for (let i = 1; i <= num; i++) {
+        concatString = concatString + `${string}`;
+        if (i === num) {
+            result = concatString;
+            concatString = '';
+            return result
+        };
+    };
 };
-
 // Do not edit below this line
 module.exports = repeatString;
