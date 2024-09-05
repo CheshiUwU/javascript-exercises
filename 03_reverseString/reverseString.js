6401661691376e77ba8ad9653621c672d2fdfb85
CheshiUwU <155390531+CheshiUwU@users.noreply.github.com>
2024-09-04 19:54:30 -0700

    all tests passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+let strLength = 0;
+let result = ''
+const reverseString = function(string) {
+    result = '';
+   strLength = string.length;
+    for (let i = strLength; i > -1; i--) {
+        if (i === 0)
+            return result;
+        let char = string.at(i -1);
+        result += `${char}`;
 
+    }
 };
-
 // Do not edit below this line
 module.exports = reverseString;
