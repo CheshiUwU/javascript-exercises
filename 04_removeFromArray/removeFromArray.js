0842bf632b2ce6fac57a3a28a03d887e04c426e4
CheshiUwU <155390531+CheshiUwU@users.noreply.github.com>
2024-09-07 15:31:47 -0700

    all tests passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-};
-
+const removeFromArray = function(array, ...args) {
+    arrLen = array.length;
+    argLen = args.length;
+    for (i = 0; i < argLen; i++) {
+        for (j = 0; j < arrLen; j++) {
+            if (array[j] === args[i]) {
+                array.splice(j, 1);
+                j--;
+            }
+        }
+    }
+    return array;
+}
 // Do not edit below this line
 module.exports = removeFromArray;
