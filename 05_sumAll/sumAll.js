54f193d3c4ce121e54aae3a5801af22fa2c77339
CheshiUwU <155390531+CheshiUwU@users.noreply.github.com>
2024-09-07 23:13:44 -0700

    all tests passed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+    result = 0;
+    if (int1 < 0 ||
+        int2 < 0 ||
+        int1 % 1 !== 0 ||
+        int2 % 1 !== 0 )  {
+            return "ERROR";
+        }
+    else if (typeof int1 === 'string' || int1 instanceof String ||
+        typeof int2 === 'string' || int2 instanceof String) {
+            return "ERROR";
+        }
+        
+        if (int1 > int2) {
+        for (i = int2; int2 <= int1; int2++) {
+            result += int2;
+        }
+        return result;
+    }
+    for (i = int1; int1 <= int2; int1++) {
+        result += int1;
+    }
+    return result;
 };
 
 // Do not edit below this line
