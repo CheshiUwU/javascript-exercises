d2392fd83eacc1400b5ec50cabfc05a55b1e8b27
CheshiUwU <155390531+CheshiUwU@users.noreply.github.com>
2024-09-12 11:43:07 -0700

    all tests passed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,48 @@
-const add = function() {
-	
+const add = function(...ints) {
+  result = 0;
+  intLen = ints.length;
+  for (i = 0; i < intLen; i++) {
+    result += ints[i]; }
+
+  return result;
 };
 
-const subtract = function() {
-	
+const subtract = function(...ints) {
+	result = ints[0];
+  intLen = ints.length;
+  for (i = 1; i < intLen; i++) {
+    result -= ints[i];
+  }
+  return result;
 };
 
-const sum = function() {
-	
+const sum = function(ints) {
+  result = 0;
+  for (i = 0; i < ints.length; i++) {
+    result += ints[i];
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(ints) {
+  result = 1;
+  for (i = 0; i < ints.length; i++) {
+    result *= ints[i];
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(int1, int2) {
+  let result = Math.pow(int1, int2);
+	return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(int) {
+  result = 1;
+  for (i = 1; i <= int; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
