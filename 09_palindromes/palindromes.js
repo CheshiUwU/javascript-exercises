fb6767f07d654d68960ce6d38814af2d11e08f12
CheshiUwU <155390531+CheshiUwU@users.noreply.github.com>
2024-09-13 19:44:28 -0700

    all tests passed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,25 @@
-const palindromes = function () {
-
+const palindromes = function (palindrome) {
+    let  arrPunctuation =  ["!", ",", "'", ".", " "]
+    reversedPalindrome = '';
+    palLength = palindrome.length;
+    arrLength = arrPunctuation.length;
+    editedPalindrome = palindrome.toLowerCase();
+    for (i = palLength; i >= 0; i--) {
+        for (j = arrLength -1; j >= 0; j--)
+        if (editedPalindrome.at(i) === arrPunctuation[j]) {
+            editedPalindrome = editedPalindrome.replace(arrPunctuation[j], "");
+            j++;
+        }
+    }
+    for (i = editedPalindrome.length -1; i >= 0; i--){
+        reversedPalindrome = reversedPalindrome + editedPalindrome.substr(i, 1);
+    }
+    if (editedPalindrome === reversedPalindrome) {
+        return true;
+    }
+    else {
+        return false;
+    } 
 };
 
 // Do not edit below this line
