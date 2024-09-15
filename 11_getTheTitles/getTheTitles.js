0fda2ea7ccf167b403782be6bf42c6fc49613a66
CheshiUwU <155390531+CheshiUwU@users.noreply.github.com>
2024-09-15 15:04:35 -0700

    removed console.log used for testing

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -3,7 +3,6 @@ const getTheTitles = function(books) {
     for (let i = 0; i < books.length; i++) {
         titles[i] = books[i].title;
     }
-    console.log(titles);
     return titles;
 }
 // Do not edit below this line
