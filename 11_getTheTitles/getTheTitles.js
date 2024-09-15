e44b45d70f3924f08f72f8187d73b8e5058f5136
CheshiUwU <155390531+CheshiUwU@users.noreply.github.com>
2024-09-15 15:02:39 -0700

    all tests completed

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,10 @@
-const getTheTitles = function() {
-
-};
-
+const getTheTitles = function(books) {
+    let titles = []
+    for (let i = 0; i < books.length; i++) {
+        titles[i] = books[i].title;
+    }
+    console.log(titles);
+    return titles;
+}
 // Do not edit below this line
 module.exports = getTheTitles;
